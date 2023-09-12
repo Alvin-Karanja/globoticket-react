ba09365187d5814b7921f661569b3477413e2abe
Alvin-Karanja <alvin2@proton.me>
2023-09-12 17:39:28 +0300

    code cleanup

--- src/Shoppingcartitem.js
+++ src/Shoppingcartitem.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { updateCart, deleteCart } from "./CartHelper";
 
 export default function Shoppingcartitem({ event }) {
-  const dispatch = useDispatch();
-
   return (
     <tr className="align-middle">
       <td>
@@ -22,7 +19,7 @@ export default function Shoppingcartitem({ event }) {
             className="w-auto"
             value={event.quantity}
             onChange={(e) =>
-              dispatch(updateCart(event.event_id, parseInt(e.target.value)))
+              updateCart(event.event_id, parseInt(e.target.value))
             }
           />
         </div>
@@ -31,7 +28,7 @@ export default function Shoppingcartitem({ event }) {
       <td>
         <button
           className="btn btn-link"
-          onClick={() => dispatch(deleteCart(event.event_id))}
+          onClick={() => deleteCart(event.event_id)}
         >
           <span className="bi bi-trash-fill font-large text-dark"></span>
         </button>
